003faced0b86f1c0d85af231f9bcaf9b9dfc5356
Matt Whitehurst <matthew.j.whitehurst@gmail.com>
2023-12-12 11:42:01 +0000

    feat: better bash

--- adder.sh
+++ adder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 # Define color variables
 GREEN='\033[0;32m'
 AMBER='\033[0;33m'
@@ -8,26 +9,46 @@ NC='\033[0m' # No Color
 
 ### FUNCTIONS ###
 
+# Coloured text
+function success( ){
+    printf "${GREEN}$1${NC}\n"
+}
+
+function warn() {
+    printf "${AMBER}$1${NC}\n"
+}
+
+function err() {
+    printf "${RED}$1${NC}\n"
+}
+
+## not a function, but 'overrides' for exit and return in diff situations.
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+        exit_adder='exit'
+    else
+        exit_adder='return'
+fi
+
 # Function to kill a Docker container by image name
-kill_container() {
+function kill_container() {
     local image_name=$1
-    echo -e "Searching for containers running image: ${image_name}"
+    warn "Searching for containers running image: ${image_name}"
 
     # Find container IDs for given image name
     container_ids=$(docker ps -q --filter ancestor=${image_name})
 
     # Check if any containers were found
     if [ -z "$container_ids" ]; then
-        echo -e "${GREEN}No running containers found for image: ${image_name}${NC}"
+        success "No running containers found for image: ${image_name}"
     else
         # Stop the containers
         echo "Stopping containers: $container_ids"
         docker stop $container_ids
 
         if [ $? -eq 0 ]; then
-            echo -e "${GREEN}Successfully stopped container(s) for image: ${image_name}${NC}"
+            success "Successfully stopped container(s) for image: ${image_name}"
         else
-            echo -e "${RED}Failed to stop container(s) for image: ${image_name}${NC}"
+            err "Failed to stop container(s) for image: ${image_name}"
         fi
     fi
 }
@@ -46,14 +67,14 @@ fi
 
 
 if [[ "$current_dir" != "adder" && "$current_dir" != "adder-backend" && "$current_dir" != "adder-frontend" ]]; then
-    echo -e "${RED}Error: This script must be run from a directory named adder, adder-backend, or adder-frontend.${NC}"
-    exit 1
+    err "Error: This script must be run from a directory named adder, adder-backend, or adder-frontend."
+    $exit_adder 1
 fi
 
 # Check if at least one argument is provided
 if [ $# -lt 1 ]; then
-    echo -e "${AMBER}Usage: $0 [backend|frontend|start|kill] [...]${NC}"
-    exit 1
+    warn "Usage: $0 [backend|frontend|start|kill] [...]"
+    $exit_adder 1
 fi
 
 # Handle the first argument
@@ -62,51 +83,63 @@ case "$1" in
         shift # Remove the first argument
         # Run backend.sh with remaining arguments
         eval ". ${adder_path}/adder-backend/backend.sh $@ &"
-        exit 0
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        $exit_adder 0
         ;;
     frontend)
         shift # Remove the first argument
         # Run frontend.sh with remaining arguments
         eval ". ${adder_path}/adder-frontend/frontend.sh $@ &"
-        exit 0
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        $exit_adder 0
 
         ;;
     start)
         # Run backend.sh and frontend.sh
-        echo -e "${GREEN} starting frontend"
+        success "starting frontend"
         eval "adder frontend"
-        echo -e "${GREEN} starting backend"
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+
+        success "starting backend"
         eval "adder backend"
-        exit 0
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        $exit_adder 0
 
         ;;
     kill)
         # Kill both frontend and backend processes
-        echo -e "${RED} killing backend"
+        err " killing backend"
         kill_container "adder-backend"
-        echo -e "${RED} killing fronend"
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        err " killing fronend"
         kill_container "adder-frontend"
-        echo -e "${NC}"
-        exit 0
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        $exit_adder 0
         ;;
     build)
-        echo -e "${AMBER} - Rebuilding adder script... ${NC}"
+        warn " - Rebuilding adder script..."
         eval ". build_adder_script.sh"
-        echo -e "${GREEN}done"
-        echo -e "${AMBER} - building backend"
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        success "done"
+        warn " - building backend NEW"
         eval "docker build -t adder-backend ${adder_path}/adder-backend"
-        echo -e "${GREEN}done"
-        echo -e "${AMBER} - building frontend"
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        success "done"
+        warn " - building frontend"
         eval "docker build -t adder-frontend ${adder_path}/adder-frontend"
-        echo -e "${GREEN}done"
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1
+        success "done"
+        ;;
+    cd)
+        success " Adder ⤵⤵⤵"
+        cd ~/adder
+        [ $? == 1 ] && err ">FAILED<" && $exit_adder 1        $exit_adder 0
         ;;
     *)
-        echo -e "${RED}Invalid argument: $1${NC}"
-        echo -e "${AMBER}Usage: $0 [backend|frontend|start] [...]${NC}"
-        exit 1
+        err "Invalid argument: $1"
+        warn "Usage: $0 [backend|frontend|start] [...]"
         ;;
 esac
 
-echo -e "${GREEN}Adder Script finished${NC}"
-
-
+success "Adder Script finished"
+$exit_adder 0
